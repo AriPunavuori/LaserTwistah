bec51c6bc8fe169e7af822cee0bfae1d4eeb60c8
kjtk <kari.knaapi@gmail.com>
2020-01-24 11:08:26 +0200

    Added Laser-Pew... esimerkit.

---
+++ Assets/Laser-Pew-Pew-Mirrors/DemoEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoEffect : MonoBehaviour {
+    Vector3 startPosition;
+    void Awake() {
+        startPosition = transform.position;
+    }
+	void Update () {
+        transform.position = startPosition + Vector3.right * Mathf.Sin(Time.time * 2.0f) * 2.0f;
+	}
+}
---
+++ Assets/Laser-Pew-Pew-Mirrors/Laser.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vectrosity;
+
+public class Laser : MonoBehaviour {
+    public float beamReach;
+    Transform beamStart;
+    VectorLine laserLine;
+
+    List<Vector3> beamPoints;
+
+    void Awake() {
+        beamPoints = new List<Vector3>();
+        beamStart = transform.Find("BeamStart");
+        laserLine = new VectorLine("LaserLine", beamPoints, 5.0f, LineType.Continuous, Joins.Fill);
+	}
+	
+	void Update () {
+        StartBeam();
+        VisualizeBeam();
+	}
+
+    void StartBeam() {
+        beamPoints.Clear();
+        ShootBeam(beamStart.position, beamStart.up, beamReach);
+    }
+
+    void ShootBeam(Vector2 origin, Vector2 dir, float maxDistance) {
+        var hit = Physics2D.Raycast(origin, dir, maxDistance);
+
+        if (hit.collider == null) {
+            CreateBeamSegment(origin, origin + dir * maxDistance);
+            return;
+        }
+
+        var go = hit.collider.gameObject;
+        CreateBeamSegment(origin, hit.point);
+        if (go.tag == "Mirror") {
+            var newDir = Vector2.Reflect(dir, hit.normal);
+            ShootBeam(hit.point - dir * 0.001f, newDir, maxDistance - hit.distance);
+        }
+        // TODO: check for laser hitting destructible things, etc.?
+    }
+
+    void CreateBeamSegment(Vector2 start, Vector2 end) {
+        if (beamPoints.Count == 0)
+            beamPoints.Add(start);
+        beamPoints.Add(end);
+    }
+
+    void VisualizeBeam() {
+        laserLine.Draw();
+        for (int i=0; i<beamPoints.Count-1; i++) {
+            Debug.DrawLine(beamPoints[i], beamPoints[i + 1]);
+        }
+    }
+}
