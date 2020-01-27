b456cc517b26e41221ee0cbf4953db6046e4d613
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-01-27 15:27:21 +0200

    Interaction

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour{
+    [SerializeField]
+    bool draggable;
+
+    public void MakeDraggable(bool d) {
+        draggable = d;
+    }
+    public bool IsDraggable() {
+        return draggable;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,42 +5,55 @@ using TMPro;
 
 public class UIManager : MonoBehaviour{
     Camera cam;
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI UIText;
+    public TextMeshProUGUI UIText2;
     public GameObject circle;
-    public float touchRadius = 0.5f;
+    public float touchRadius = .5f;
+    float speed = .25f;
     Dictionary<int, Rigidbody2D> drags = new Dictionary<int, Rigidbody2D>();
+    Dictionary<int, Vector2[]> touchVel = new Dictionary<int, Vector2[]>();
 
     private void Start() {
         cam = Camera.main;
+        Application.targetFrameRate = 60;
     }
 
     void Update() {
+        // Loop through touches
         for(int i = 0; i < Input.touchCount; i++) {
             var t = Input.touches[i];
             var s = cam.ScreenToWorldPoint(t.position);
             s.z = 0;
+
+            // Check if object is draggable, add to dictionary and make array for velEst
             if (t.phase == TouchPhase.Began) {
                 var col = Physics2D.OverlapCircleAll(s, touchRadius);
                 foreach(var c in col) {
-                    
-                    var go = Instantiate(circle, s, Quaternion.identity);
-                    text.text = "osui " + i;
-
-                    // if thing is draggable
-                    //drags.Add(t.fingerId, rb);
+                    if(c.GetComponent<Interaction>().IsDraggable()) {
+                        drags.Add(t.fingerId, c.GetComponent<Rigidbody2D>());
+                        touchVel.Add(t.fingerId, new Vector2[3]);
+                    }
                 }
             }
 
-            // if (drags.ContainsKey(t.fingerId)) 
+            // Give rb velocity and remove from dictionarys
+            else if(t.phase == TouchPhase.Ended) {
+                var ar = touchVel[t.fingerId];
+                var velEst = (ar[0] + ar[1] + ar[2]) / 3;
+                drags[t.fingerId].velocity = velEst * speed;
+                touchVel.Remove(t.fingerId);
+                drags.Remove(t.fingerId);
+            }
 
+            // Move object to finger position
+            else if (drags.ContainsKey(t.fingerId)) {
+                drags[t.fingerId].velocity = Vector2.zero;
+                drags[t.fingerId].MovePosition(s);
+                var ar = touchVel[t.fingerId];
+                ar[2] = ar[1];
+                ar[1] = ar[0];
+                ar[0] = t.deltaPosition;
+            }
         }
-        //if(Input.GetKeyDown(KeyCode.Mouse0)) {
-        //    var s = cam.ScreenToWorldPoint(Input.mousePosition);
-        //    s.z = 0;
-        //    var go = Instantiate(circle, s, Quaternion.identity);
-        //    text.text = "osui ";
-        //}
     }
-
-
 }
