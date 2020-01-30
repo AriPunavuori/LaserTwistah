d6c146d051df46670bdd13425bd9e4ed44b2fed2
Ari Punavuori <ari.punavuori@opisto.hkro>
2020-01-30 17:00:15 +0200

    Laser 1.0

--- Assets/Laser-Pew-Pew-Mirrors/Laser.cs
+++ Assets/Laser-Pew-Pew-Mirrors/Laser.cs
@@ -37,13 +37,17 @@ public class Laser : MonoBehaviour
         beamPoints.Clear();
         objectsHit = new List<GameObject>();
         beamDistances = new List<float>();
-        ShootBeam(beamStart.position, beamStart.up, currentMaxDistance);
+        ShootBeam(beamStart.position, beamStart.up, currentMaxDistance, 0);
     }
 
-    void ShootBeam(Vector2 origin, Vector2 dir, float maxDistance) {
+    void ShootBeam(Vector2 origin, Vector2 dir, float maxDistance, int nBeamSegment) {
         var hit = Physics2D.Raycast(origin, dir, maxDistance);
 
         if(hit.collider == null) {
+            if (nBeamSegment < previousObjectsHit.Count) {
+                currentMaxDistance = CalculateMaxDistance(nBeamSegment);
+                maxDistance = previousBeamDistances[nBeamSegment];
+            }
             CreateBeamSegment(origin, origin + dir * maxDistance);
             return;
         }
@@ -52,15 +56,31 @@ public class Laser : MonoBehaviour
             return;
         }
 
-        beamDistances.Add(hit.distance);
-
         var go = hit.collider.gameObject;
+
+        if (nBeamSegment < previousObjectsHit.Count && go != previousObjectsHit[nBeamSegment]) {
+            var prevD = previousBeamDistances[nBeamSegment];
+            //maxDistance = previousBeamDistances[nBeamSegment];
+            if (prevD < hit.distance) {
+                currentMaxDistance = CalculateMaxDistance(nBeamSegment);
+                CreateBeamSegment(origin, origin + dir * prevD);
+            } else {
+                currentMaxDistance = CalculateMaxDistance(nBeamSegment)
+                    - previousBeamDistances[nBeamSegment]
+                    + hit.distance;
+                CreateBeamSegment(origin, origin + dir * hit.distance);
+            }
+            return;
+        }
+
         CreateBeamSegment(origin, hit.point);
 
         objectsHit.Add(go);
+        beamDistances.Add(hit.distance);
+
         if(go.tag == "Mirror") {
             var newDir = Vector2.Reflect(dir, hit.normal);
-            ShootBeam(hit.point - dir * 0.001f, newDir, maxDistance - hit.distance);
+            ShootBeam(hit.point - dir * 0.001f, newDir, maxDistance - hit.distance, nBeamSegment + 1);
         }
 
         IDamageable damageable = go.GetComponentInParent<IDamageable>();
@@ -82,19 +102,28 @@ public class Laser : MonoBehaviour
         }
     }
 
+    float CalculateMaxDistance(int beamCount) {
+        var dist = 0f;
+        for(int i = 0; i < beamCount; i++) {
+            dist += beamDistances[i];
+        }
+        dist += previousBeamDistances[beamCount];
+        return dist;
+    }
+
     void CompareBeams() {
         // compare objectsHit to previousObjectsHit
-        for(int i = 0; i < previousObjectsHit.Count; i++) {
-            if (i >= objectsHit.Count || objectsHit[i] != previousObjectsHit[i]) {
-                var dist = 0f; ;
-                for(int j = 0; j < i; j++) {
-                    dist += beamDistances[j];
-                }
-                dist += previousBeamDistances[i];
-                currentMaxDistance = dist;
-                break;
-            }
-        }
+        //for(int i = 0; i < previousObjectsHit.Count; i++) {
+        //    if (i >= objectsHit.Count || objectsHit[i] != previousObjectsHit[i]) {
+        //        var dist = 0f; ;
+        //        for(int j = 0; j < i; j++) {
+        //            dist += beamDistances[j];
+        //        }
+        //        dist += previousBeamDistances[i];
+        //        currentMaxDistance = dist;
+        //        break;
+        //    }
+        //}
         // if changed clamp currentMaxDistance
         previousBeamDistances = beamDistances;
         previousObjectsHit = objectsHit;
